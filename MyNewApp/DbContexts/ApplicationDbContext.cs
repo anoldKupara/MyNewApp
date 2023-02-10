82855c4a0139f39d19b56bfaf8eef7bfe93938b9
Anold Kupara <kuparaanold20@gmail.com>
2023-02-10 12:32:25 +0200

    added the ApplicationDbContext class

---
+++ MyNewApp/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNewApp.Models;
+
+namespace MyNewApp.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
