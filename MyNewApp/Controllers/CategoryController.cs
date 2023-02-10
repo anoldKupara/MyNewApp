e63578661077ff4a8b6351579807bbe3b4224d4c
Anold Kupara <kuparaanold20@gmail.com>
2023-02-10 14:56:03 +0200

    added an implementation to retrieve all categories as well the view

--- MyNewApp/Controllers/CategoryController.cs
+++ MyNewApp/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyNewApp.DbContexts;
 
 namespace MyNewApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
+            var categoryList = _dbContext.Categories.ToList();
             return View();
         }
     }
