20bbc4996e9d8ba3430d60917e011167f7bfcc68
Anold Kupara <kuparaanold20@gmail.com>
2023-02-13 12:34:40 +0200

    added code to create a new category

--- MyNewApp/Controllers/CategoryController.cs
+++ MyNewApp/Controllers/CategoryController.cs
@@ -16,12 +16,26 @@ namespace MyNewApp.Controllers
         {
             IEnumerable<Category> categoryList = _dbContext.Categories;
             return View(categoryList);
-        }
+        }   
 
         // GET - Create
         public IActionResult Create()
         {
             return View();
+        }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
