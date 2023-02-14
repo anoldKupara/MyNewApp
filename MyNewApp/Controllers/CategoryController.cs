1acc8415388606e021fcd66ba3801f33f9651ebf
Anold Kupara <kuparaanold20@gmail.com>
2023-02-14 11:17:07 +0200

    added Edit functionality the categories

--- MyNewApp/Controllers/CategoryController.cs
+++ MyNewApp/Controllers/CategoryController.cs
@@ -56,5 +56,54 @@ namespace MyNewApp.Controllers
             }
             return View(category);
         }
+
+
+        // GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
+            }
+
+            if (category.Name == null)
+            {
+                ModelState.AddModelError("Name", "Name cannot be null");
+            }
+
+            if (category.Name.Length > 100)
+            {
+                ModelState.AddModelError("Name", "Name cannot be longer than 100 characters");
+            }
+
+            if (category.Name.Length < 3)
+            {
+                ModelState.AddModelError("Name", "Name cannot be shorter than 3 characters");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
--- MyNewApp/Models/Category.cs
+++ MyNewApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyNewApp.Models
 {
@@ -8,6 +9,8 @@ namespace MyNewApp.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("DisplayOrder")]
+        [Range(1, int.MaxValue, ErrorMessage = "Display order for category must be greater than 0")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
