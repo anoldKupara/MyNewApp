71d8881bb0b71c30ea5265cb338976a4694bcfbd
Anold Kupara <kuparaanold20@gmail.com>
2023-02-15 11:43:08 +0200

    added the IRepository interface

---
+++ MyNewApp.DataAccess/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewApp.DataAccess.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(int id);
+        void Remove(T entity);
+    }
+}
