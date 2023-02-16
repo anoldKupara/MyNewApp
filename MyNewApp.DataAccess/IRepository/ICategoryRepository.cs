51d6e7adaa651d992e7c6902fad1a1c0add7529e
Anold Kupara <kuparaanold20@gmail.com>
2023-02-16 15:12:14 +0200

    added the ICategoryRepository interface

---
+++ MyNewApp.DataAccess/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyNewApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewApp.DataAccess.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
