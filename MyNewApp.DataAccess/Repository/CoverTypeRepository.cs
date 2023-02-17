19dc0835031918d9591fa445cef9c92891bf3400
Anold Kupara <kuparaanold20@gmail.com>
2023-02-17 12:02:56 +0200

    added the CoverTypeRepository

---
+++ MyNewApp.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,30 @@
+﻿using MyNewApp.DataAccess.DbContexts;
+using MyNewApp.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewApp.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoverTypeRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Update(CoverType obj)
+        {
+            _dbContext.Update(obj);
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
