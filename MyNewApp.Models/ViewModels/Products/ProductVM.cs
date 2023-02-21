81316b66732e929ce793f4a4b10a58f6ab2513bb
Anold Kupara <kuparaanold20@gmail.com>
2023-02-21 12:47:35 +0200

    added ProductVM class

---
+++ MyNewApp.Models/ViewModels/Products/ProductVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyNewApp.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewApp.Models.ViewModels.Products
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+    }
+}
