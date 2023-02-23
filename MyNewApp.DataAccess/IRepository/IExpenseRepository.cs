dbcadb94501da5bff3d4a768d6f74545585053c2
Anold Kupara <kuparaanold20@gmail.com>
2023-02-23 15:38:03 +0200

    added the IExpenseRepository interface

---
+++ MyNewApp.DataAccess/IRepository/IExpenseRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyNewApp.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewApp.DataAccess.IRepository
+{
+    public interface IExpenseRepository : IRepository<Expense>
+    {
+        void Update(Expense obj);
+        void Save();
+    }
+}
