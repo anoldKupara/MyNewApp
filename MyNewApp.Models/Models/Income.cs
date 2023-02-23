a7c9a52d7ca0c5cba779004a634c6c17be21cc18
Anold Kupara <kuparaanold20@gmail.com>
2023-02-23 14:38:51 +0200

    added Income model class

---
+++ MyNewApp.Models/Models/Income.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewApp.Models.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int CategoryId { get; set; }   // foreign key
+        public Category Category { get; set; }   // navigation property
+    }
+}
